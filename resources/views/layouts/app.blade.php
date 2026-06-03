ae3af8624c32e03a7dc97eca2c39eaf3ff52f16b
LHC Data Deploy <deploy@lhc.local>
2026-06-03 20:40:59 +0100

    Add favicon inclusion to access and app layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'LHC Catalogue Staging')</title>
+    @include('partials.favicon')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="app-body {{ \App\Support\AccessMode::isDataEntry() ? 'is-data-entry-mode' : '' }}">
---
+++ resources/views/partials/favicon.blade.php
@@ -0,0 +1,2 @@
+<link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml" sizes="any">
+<meta name="theme-color" content="#1d6f5f">
